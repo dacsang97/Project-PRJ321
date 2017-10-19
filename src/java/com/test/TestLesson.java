0a6a3464b78199d068f920e65f5e05fb5730fede
TuanPV <32923254+aupous@users.noreply.github.com>
2017-10-20 02:29:04 +0700

    Edited 3

--- src/java/com/test/TestLesson.java
+++ src/java/com/test/TestLesson.java
@@ -20,17 +20,24 @@ public class TestLesson extends TestCase{
         assertEquals(true, l.createLesson("kaka", 3, 3));
     }
     
+    //test ID existed
     public void testGetLesson() throws Exception {
         int lid = 1;
         assertEquals("1 Progetest1 2 3", l.getLesson(lid).toString());
     }
     
+    //test ID does not existed
+    public void testGetLesson1() throws Exception {
+        int lid = 15;
+        assertEquals(null, l.getLesson(lid));
+    }
+    
     public void testUpdateLesson() throws Exception {
-        assertEquals(true, l.updateLesson(10, "hihi", -1, -1));
+        assertEquals(true, l.updateLesson(9, "hihi", -1, -1));
     }
     
     public void testDeleteLesson() throws Exception {
-        assertEquals(true, l.deleteLesson(15));
+        assertEquals(false, l.deleteLesson(15));
     }
     public static void main(String[] args) {
         junit.textui.TestRunner.run(new TestSuite(TestLesson.class));
